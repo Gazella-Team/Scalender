07bc35400641023b731409dd62c34d3c7a7907ee
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-19 17:52:32 +0100

    index

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
             <div className="w-[94%] mx-auto max-w-[1400px] text-white flex items-center justify-between">
                 <h1 className="text-[20px] font-semibold uppercase">scalender</h1>
                 <div className="flex items-center gap-6 font-medium">
-                    {links.map((v, index) => <Link className="text-white" href={v.path}>{v.name}</Link>)}
+                    {links.map((v, index) => <Link key={index} className="text-white" href={v.path}>{v.name}</Link>)}
                 </div>
                 <div>
                     <Link className="px-7 py-3 rounded-full text-sm font-semibold bg-gradient-to-br from-blue-600 via-blue-500 to-indigo-700" href={"/"}>Book møde</Link>
