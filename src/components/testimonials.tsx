091475fbbac6e8f3057853f47d4ca931cb88e25f
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-28 00:01:46 +0100

    kg

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,7 +1,7 @@
 const testimonialsData = [
-    {heading:"Amazing Service!", description: "I've been working with Kasper and Niclas for a couple of months. In our first month we averaged about 5-7 sales calls per week which was amazing ‍ So far we've spent about £3,000 and made £18,000 with them ‍ They are two genuine young guys trying to look after their clients. I would highly recommmed giving them a shot", name:"Ventur", image:"/people/two.jpg", position:"CEO"},
-    {heading:"Phenomenal Service", description: "Before partnering with Client Complex I had worked with and heard of many outreach agencies, non of whom were able to deliver results like Kasper & Niclas. So far it's been very impressive what the guys have been able to do - also communications has been super - they're simply professionals. Overall I couldn't recommend them more!", name:"Gizeno", image:"/people/two.jpg", position:"CEO"},
-    {heading:"Phenomenal Service", description: "If you know anything about ecom outreach, you know how difficult it is to get quality webshop owners on sales calls - Scalender has somehow cracked the code. Big recommendation from me and & the team", name:"COAD", image:"/people/two.jpg", position:"CEO"},
+    {heading:"Amazing Service!", description: "I've been working with Kasper and Niclas for a couple of months. In our first month we averaged about 5-7 sales calls per week which was amazing ‍ So far we've spent about £3,000 and made £18,000 with them ‍ They are two genuine young guys trying to look after their clients. I would highly recommmed giving them a shot", name:"Ventur", image:"/people/Ventur CEO.jpg", position:"CEO"},
+    {heading:"Impressive results", description: "Before partnering with Client Complex I had worked with and heard of many outreach agencies, non of whom were able to deliver results like Kasper & Niclas. So far it's been very impressive what the guys have been able to do - also communications has been super - they're simply professionals. Overall I couldn't recommend them more!", name:"Gizeno", image:"/people/Gustav CEO.jpg", position:"CEO"},
+    {heading:"Big recommendation!", description: "If you know anything about ecom outreach, you know how difficult it is to get quality webshop owners on sales calls - Scalender has somehow cracked the code. Big recommendation from me and & the team", name:"COAD", image:"/people/Gizeno CEO.jpg", position:"CEO"},
 ]
 
 export default function Testimonials() {
@@ -22,7 +22,7 @@ export default function Testimonials() {
 const TestimonialCard = (props:any) => {
     return (
         <div  className="p-10 border-[1px] rounded-xl flex flex-col justify-between">
-            <img className="w-[100px] mb-[20px]" src="/sliderlogos/meta.png"></img>
+            <img className="w-[100px] mb-[20px]" src="/5-star.webp"></img>
             <div className="flex-1">
                 <h1 className="text-xl font-semibold mb-[14px]">{props.Obj.heading}</h1>
                 <p className="text-gray-600 font-regular mb-12">{props.Obj.description}</p>
