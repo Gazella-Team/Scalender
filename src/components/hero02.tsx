6a0a3c0913298323991d9b36293c3fab02e42874
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-29 16:25:42 +0100

    bg

--- src/components/hero02.tsx
+++ src/components/hero02.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 export default function Hero02() {
     return (
-        <section style={{backgroundImage: 'linear-gradient(to bottom, rgba(74,0,222,0.7825323879551821) 0%, rgba(0,0,129,0.1)), url("/bg.jpg")'}} className="bg-center bg-cover">
+        <section style={{backgroundImage: 'linear-gradient(to bottom, rgba(0,10,93,0.925323879551821) 0%, rgba(0,0,129,0.2)), url("/bg.jpg")'}} className="bg-center bg-cover">
             <Navbar />
             <div className="text-center text-white py-[70px] pt-[10px] flex flex-col justify-center ">
                 <Reveal delayTime={0.2}>
