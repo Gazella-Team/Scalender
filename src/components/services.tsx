a4830eeb3a9fc65fc7f3b863ec1c88cbf4b1057c
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-01 09:31:22 +0200

    lr

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -21,14 +21,14 @@ export default function Services() {
                             <img className="w-[50px]" src="/services/linkedin.png"></img>
                         </div>
                         <h1 className="text-[22px] font-medium mb-5">LinkedIn Outreach</h1>
-                        <p className="text-gray-600">LinkedIn is still an amazing tool for prospecting. Utilizing the same AI-methods as we do with cold email, we deliver genuine and human messages to your ideal prospects on LinkedIn, securing a very high booking-rate despite the limitations of volume on LinkedIn’s platform. We set up complex automated messaging flows that take into account any potential situation that might occur in the prospects journey.</p>
+                        <p className="text-gray-600">LinkedIn is still an amazing tool for prospecting. Utilizing the same AI-methods as we do with cold email, we deliver genuine and human messages to your ideal prospects on LinkedIn, securing a very high booking-rate despite the limitations of volume on LinkedIn&apos;s platform. We set up complex automated messaging flows that take into account any potential situation that might occur in the prospects journey.</p>
                     </div>
                     <div style={{background:"linear-gradient(338deg, rgba(174,186,247,1) 2%, rgba(255,255,255,1) 48%, rgba(255,255,255,1) 100%)"}} className="bg-white border-[1px] p-10 rounded-xl text-center">
                         <div className="p-3 rounded-xl mb-8 border-[1px] inline-block">
                             <Phone size={45} />
                         </div>
                         <h1 className="text-[22px] font-medium mb-5">Cold Calling</h1>
-                        <p className="text-gray-600">Our approach to cold calling is both strategic and personalized. We take the time to understand your target audience, crafting compelling scripts that resonate with their needs and interests. Our skilled team of cold callers is trained to engage prospects effectively, overcoming objections and generating qualified leads. By outsourcing your cold calling needs to us, you get a dedicated team that's going to learn everything about your company and audience.</p>
+                        <p className="text-gray-600">Our approach to cold calling is both strategic and personalized. We take the time to understand your target audience, crafting compelling scripts that resonate with their needs and interests. Our skilled team of cold callers is trained to engage prospects effectively, overcoming objections and generating qualified leads. By outsourcing your cold calling needs to us, you get a dedicated team that&apos;s going to learn everything about your company and audience.</p>
                     </div>
                 </div>
             </div>
